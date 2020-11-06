a7a61458cefbb670be57ada8a77a9a0db1a28baa
emma-hemmerle <73693916+emma-hemmerle@users.noreply.github.com>
2020-11-06 11:23:59 +0100

    Create R_code_plot_ecol_var.R

---
+++ R_code_plot_ecol_var.R
@@ -0,0 +1,53 @@
+# title of the script: R code for plotting the relationship between or among several ecological variables
+# today, we are not inventing data, we are going to be using data from a package
+# e.g. rasterdiv package built by some of Duccio's students to calculate biodiversity directly from remote sensing
+# today, we are going to make use of a package called 'sp' built to look at spatial data
+# page with all the info about the package: https://cran.r-project.org/web/packages/sp/index.html
+# you can use the R documentation webpage to look up any function: https://www.rdocumentation.org/
+
+# How to install the package? 
+# we are going to use the function "install.packages", and the argument will be the name of the package
+install.packages("sp")
+
+library(sp)
+
+# another function that does it same thing (attach/start using the package) is called 'require'
+# for the library function, we no longer have to use brackets around sp, because the package is now already inside of R (it wasn't before we installed it)
+# data function is used to recall datasets
+
+data(meuse)
+
+# look inside the dataset
+meuse
+# the data set can be seen 
+# View function: warning, R is case sensitive, and the View function has a capital 'V'
+# the View function doesn't automatically work on R, you need to download Quartz: https://www.xquartz.org
+View(meuse)
+# with the head function, you will only see the first couple of rows
+head(meuse)
+
+#finding the means of all of the variables
+# by using the summary function 
+summary(meuse)
+
+# now we want to make a plot
+# looking at the relationship between two variables
+# zinc on Y axis, Cadmium on X axis
+attach(meuse)
+plot(cadmium,zinc)
+
+# another way to go about it is the following:
+# $ symbol is used in R to link different parts
+plot(meuse$cadmium,meuse$zinc)
+
+#plotting all the variables, against all the other variables (pair-wise plots for all the variable)
+#you can use scatter plot matrices with the pairs function
+pairs(meuse)
+
+#question to answer before next lecture
+# pairing only the elelents part of the dataset (how do you do that)? 
+# so instead of pairing all the variables (you only pair cadmium, copper, zinc and lead). 
+
+subsetmeuse<-c("cadmium","copper","zinc","lead")
+subsettedmeuse<-meuse[subsetmeuse]
+pairs(subsettedmeuse)
