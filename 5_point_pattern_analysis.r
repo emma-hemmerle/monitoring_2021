81b98b08d138e54fc0382f61f602d17782ceaad2
emma-hemmerle <73693916+emma-hemmerle@users.noreply.github.com>
2020-11-30 11:07:23 +0100

    Update 5_point_pattern_analysis.r

--- 5_point_pattern_analysis.r
+++ 5_point_pattern_analysis.r
@@ -4,6 +4,8 @@ setwd("/Users/emmahemmerle/Documents/EDU/UniBo/Ecosystem Monitoring/R Lab")
 library(spatstat)
 library(rgdal)
 covid<-read.csv("covid_agg.csv", header=TRUE)
+# or: 
+covid<-read.table("covid_agg.csv", header=TRUE, sep=",")
 #checking that everything is ok: 
 head(covid)
 attach(covid)
@@ -30,4 +32,27 @@ coastlines <- readOGR("ne_10m_coastline.shp")
 plot(coastines, add=T)
 
 ### we can re-sample the coastlines data so that it takes less time to load 
+
+
+
+##### working on Leonardo Zabotti's Data 
+
+leo<-read.table("dati_zabotti.csv", header= TRUE, sep=",")
+library(spatstat)
+library(rgdal)
+# we are going to build a density map (and before that we need to build the ppp)
+# Then we will build the smooth map (i.e. interpolation)
+# for the interpolation we will be using marks to say which values we want to interpret
+attach(leo)
+summary(leo)
+# we can look at the maximum and minimum values of x and y coordinates, and we will make a ppp that is slightly wider
+leo_ppp<-ppp(x,y,c(2300000,2325000),c(5000000,5045000))
+plot(leo_ppp)
+density_map<-density(leo_ppp)
+plot(density_map)
+points(leo_ppp)
+
+
+
+###We are going to save the R workspace!! 
 
