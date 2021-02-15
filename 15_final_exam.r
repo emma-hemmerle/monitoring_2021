1e948deb1955bdf0b439d68bc1c1fa42d545d220
emma-hemmerle <73693916+emma-hemmerle@users.noreply.github.com>
2021-02-15 12:28:50 +0100

    Update and rename 14_final_exam.r to 15_final_exam.r

--- 15_final_exam.r
+++ 15_final_exam.r
@@ -19,5 +19,20 @@ library(geosphere)
 
 
 # downloading species occurence data from GBIF for Bombus affinis
+# cleaning up occurence data into clean .csv file 
+
+gbif <- read.csv('./GBIF/occurrences_bombus_affinis_min.csv',header=TRUE, sep=',')
+head(gbif)
+plot(decimalLatitude, decimalLongitude, pch=17, cex=0.35, col='blue')
+
+# downloading world's borders dataset 
+countries <- readOGR('./TM_WORLD_BORDERS_SIMPL-0.3/TM_WORLD_BORDERS_SIMPL-0.3.shp')
+plot(countries, col='gray')
+#adding species occurence data on top of world map
+points(gbif$decimalLongitude,gbif$decimalLatitude,col='mediumseagreen',pch=17,cex=0.35)
+
+
+
+
 
 
