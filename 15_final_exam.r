ced56ebb2bd4e5259529077266852d366a649e7b
emma-hemmerle <73693916+emma-hemmerle@users.noreply.github.com>
2021-02-15 15:01:00 +0100

    Update 15_final_exam.r

--- 15_final_exam.r
+++ 15_final_exam.r
@@ -29,7 +29,21 @@ plot(decimalLatitude, decimalLongitude, pch=17, cex=0.35, col='blue')
 countries <- readOGR('./TM_WORLD_BORDERS_SIMPL-0.3/TM_WORLD_BORDERS_SIMPL-0.3.shp')
 plot(countries, col='gray')
 #adding species occurence data on top of world map
-points(gbif$decimalLongitude,gbif$decimalLatitude,col='mediumseagreen',pch=17,cex=0.35)
+points(gbif$decimalLongitude,gbif$decimalLatitude,col='cyan',pch=20,cex=0.25)
+
+# rescaling map over area of interest 
+recordsSpatial <- SpatialPointsDataFrame(coords=cbind(gbif$decimalLongitude, gbif$decimalLatitude),data=gbif,proj4string=CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
+plot(recordsSpatial, col='white')
+plot(countries, add=TRUE, col='gray')
+points(recordsSpatial, pch=20, col='black',cex=0.25)
+
+# downloading scaled species distribution map 
+pdf("bombus_affinis_distrib_map.pdf")
+plot(recordsSpatial, col='white')
+plot(countries, add=TRUE, col='gray')
+points(recordsSpatial, pch=20, col='black',cex=0.25)
+dev.off()
+
 
 
 
