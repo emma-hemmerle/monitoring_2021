f4f6c5c714e2acafc5bef1fb0b3fc5138f55b90b
emma-hemmerle <73693916+emma-hemmerle@users.noreply.github.com>
2021-02-15 17:00:16 +0100

    Update 15_final_exam.r

--- 15_final_exam.r
+++ 15_final_exam.r
@@ -45,6 +45,62 @@ points(recordsSpatial, pch=20, col='black',cex=0.25)
 dev.off()
 
 
+# Saving work done so far:
+save.image('SDM project', compress=TRUE)
+
+#staring off from where we had left off
+setwd("/Users/emmahemmerle/Documents/EDU/UniBo/S1/Ecosystem Monitoring/R Lab/Final Exam")
+load("/Users/emmahemmerle/Documents/EDU/UniBo/S1/Ecosystem Monitoring/R Lab/Final Exam/SDM Project")
+
+
+# Using Historical Bioclim Data 
+# Downloading 2.5 min data from worldclim 
+
+#looking the mean annual temperature layer
+mat <- raster('./WORLDCLIM/2.5m_bio_1970_2000/wc2.1_2.5m_bio_1.tif')
+mat 
+plot(mat,main="Mean Annual Temperature 1970-2000")
+
+# downloading mean annual temperature map
+pdf("Mean Annual Temp 1970-2000.pdf")
+plot(mat,main="Mean Annual Temperature 1970-2000")
+dev.off()
+
+# creating a stack with all of the variables we have identified as relevant for our study 
+WC01<-raster('./WORLDCLIM/2.5m_bio_1970_2000/wc2.1_2.5m_bio_1.tif')
+WC03<-raster('./WORLDCLIM/2.5m_bio_1970_2000/wc2.1_2.5m_bio_3.tif')
+WC04<-raster('./WORLDCLIM/2.5m_bio_1970_2000/wc2.1_2.5m_bio_4.tif')
+WC05<-raster('./WORLDCLIM/2.5m_bio_1970_2000/wc2.1_2.5m_bio_5.tif')
+WC12<-raster('./WORLDCLIM/2.5m_bio_1970_2000/wc2.1_2.5m_bio_12.tif')
+WC15<-raster('./WORLDCLIM/2.5m_bio_1970_2000/wc2.1_2.5m_bio_15.tif')
+WC17<-raster('./WORLDCLIM/2.5m_bio_1970_2000/wc2.1_2.5m_bio_17.tif')
+bio_clim<-stack(WC01,WC03,WC04,WC05,WC12,WC15,WC17)
+pdf("bio_clim_stack_1970_2000.pdf")
+plot(bio_clim)
+dev.off()
+
+# clipping the bioclim rasters to fit our study area 
+dir.create('./WORLDCLIM/2.5m_bio_1970_2000/Study Region', recursive=TRUE, showWarnings=FALSE)
+for (i in c(1,3,4,5,12,15,17) {print(paste('Clipping current WORLDCLIM raster', i))
+                 flush.console()    
+                 # get the world raster   
+                 rast <- raster(paste0('./WORLDCLIM/2.5m_bio_1970_2000/wc2.1_2.5m_bio_',ifelse(i < 10, '0', ''), i, '.tif'))
+                 # crop the raster and update metadata
+                 rast <- crop(rast, studyExtent)
+                 rast <- setMinMax(rast)
+                 projection(rast) <-'+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'
+                 # save raster
+                 writeRaster(rast,paste0('./WORLDCLIM/2.5m_bio_1970_2000/Study Region/WC',ifelse(i < 10, '0', ''), i), format='GTiff', datatype='INT2S',overwrite=TRUE)
+                }
+current <- stack(list.files('./WORLDCLIM/2.5m_bio_1970_2000/Study Region',full.names=TRUE,pattern='.tif'))
+plot(current)
+
+
+rast <- crop(./WORLDCLIM/2.5m_bio_1970_2000/wc2.1_2.5m_bio_1.tif, studyExtent)
+rast <- setMinMax(rast)
+projection(rast) <-'+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'
+     
+
 
 
 
